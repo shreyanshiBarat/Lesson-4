383e0bfeaf96d18fc4b98f6d3a40aac7a38c933f
shreyanshiBarat <shreyanshi6972@ggn.hxls.org>
2026-05-15 19:56:15 +0530

    Lesson 4

---
+++ Activity3.py
@@ -0,0 +1,17 @@
+# 1) Ask the user to enter marks for 4 subject : math, english, science, and hindi.
+math= 9
+english= 10
+science= 8
+hindi=7
+# 2) Add all 4 subjects marks and score the total in 'sum'.
+sum= math + english + science + hindi 
+# 3) Print the total marks stored in 'sum'
+print(sum, "is the total marks")
+# 4 ) Calculate the percentage:
+# - Divide 'sum' by 400 (total maximum marks for 4 subjects, assuming each is out of 100)
+# - Multiply the result by 100
+sum/400*100
+perc= sum
+
+# Store the final value in 'perc'
+print(perc, "is the percentage")
---
+++ Activty1.py
@@ -0,0 +1,18 @@
+# 1) Store the values for each tree in seprete variables: tree1, tree2, tree3, tree4, tree5
+tree1= 1
+tree2= 2
+tree3= 3
+tree4= 4
+tree5= 5
+
+# 2) Add all 5 tree values and store the result in 'sun'
+sum= tree1 + tree2 + tree3 + tree5 
+
+# 3) Print the total ('sum') of all 5 trees
+print(sum, " is the total of all trees")
+
+# 4) Divide 'sun' by 5 to find the average and store it in 'average'
+average= sum/5
+
+# 5) Print the average ('average') of all 5 trees
+print(average, " is the average of all trees")
