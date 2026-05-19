5712d451e5e9220a7007f47384679b3d8aa4067d
shreyanshiBarat <shreyanshi6972@ggn.hxls.org>
2026-05-19 20:33:40 +0530

    Lesson 4

--- Homework.py
+++ Homework.py
@@ -1,8 +1,6 @@
-Square_root =int(input("Please enter a number for a square root :"))
-square_1 = Square_root**100
-square_2 =(Square_root**100)//50
-square_3 = ((Square_root**100)%50)//10
+import math
 
-print( "square roots of 100 " , square_1)
-print( "square roots of 50 " , square_2)
-print("square roots of 10 " , square_3)
+num = float(input("Enter a number: "))
+print(f"The square root is: {math.sqrt(num)}")
+
+
